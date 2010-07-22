4fc2f7d580064fa11588d167f743445c87322dc4
tibor.somodi <tibor.somodi@inepex.com>
2010-07-22 19:23:02 +0200

    added copyright and license notes translated doc to english

--- com.inepex.classtemplater.plugin/src/com/inepex/classtemplater/plugin/ui/LogiSelectionEvent.java
+++ com.inepex.classtemplater.plugin/src/com/inepex/classtemplater/plugin/ui/LogiSelectionEvent.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright:
+ * 2010 Tibor Somodi, Inepex, Hungary, http://www.inepex.com
+ * License:
+ * EPL: http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package com.inepex.classtemplater.plugin.ui;
 
 import java.util.List;
