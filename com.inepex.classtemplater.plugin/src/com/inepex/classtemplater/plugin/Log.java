2fca93da29a7c762019fc9680d9213b85c58477e
tibor.somodi <tibor.somodi@inepex.com>
2010-08-16 22:10:16 +0200

    test modification

--- com.inepex.classtemplater.plugin/src/com/inepex/classtemplater/plugin/Log.java
+++ com.inepex.classtemplater.plugin/src/com/inepex/classtemplater/plugin/Log.java
@@ -5,9 +5,11 @@ import org.eclipse.core.runtime.Status;
 
 public class Log {
 
+	//Log
+	
 	public static void log(String message){
 		Platform.getLog(Platform.getBundle("com.inepex.classtemplater.plugin")).log(
-				new Status(Status.WARNING, "com.inepex.classtemplater.plugin", message));
+				new Status(Status.WARNING, "com.inepex.classtemplater.plugin", message));	
 	}
 	
 }
