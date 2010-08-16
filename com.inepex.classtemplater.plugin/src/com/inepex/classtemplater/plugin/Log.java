04ef7b6ccc26852800d5a73328413d2e41dbbb37
tibor.somodi <tibor.somodi@inepex.com>
2010-08-16 21:45:46 +0200

    handled problem with non-string annotation attributes

---
+++ com.inepex.classtemplater.plugin/src/com/inepex/classtemplater/plugin/Log.java
@@ -0,0 +1,13 @@
+package com.inepex.classtemplater.plugin;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+
+public class Log {
+
+	public static void log(String message){
+		Platform.getLog(Platform.getBundle("com.inepex.classtemplater.plugin")).log(
+				new Status(Status.WARNING, "com.inepex.classtemplater.plugin", message));
+	}
+	
+}
